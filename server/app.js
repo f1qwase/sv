87aaa13dda7001777513a0fd4382b3d487c47fa7
timofeyka <timofeyka@gmail.com>
2013-04-19 12:18:09 +0400

    express, mysql installed

--- server/app.js
+++ server/app.js
@@ -1,8 +1,35 @@
-var http = require("http")
 var orm = require("orm")
+var express = require("express")
+var fs = require("fs")
 
-http.createServer(function(request, response) {
-	response.writeHead(200, {"Content-Type": "text/plain"})
-	response.write("Hello World")
-	response.end()
-}).listen("8237")
+var app = express()
+
+var config = JSON.parse(fs.readFileSync("config.json"))
+
+console.log("mysql://" + config.dbUser + ":" + config.dbPassword + "@" + config.dbHost + "/" + config.dbDatabase)
+
+app.use(orm.express("mysql://" + config.dbUser + ":" + config.dbPassword + "@" + config.dbHost + "/" + config.dbDatabase, {
+    define: function (db, models) {
+        models.file = db.define("file", {
+        	name: String,
+			url: String,
+			semester: Number,
+			type: ["лекции", "задание", "готовая работа", "вопросы", "другое"]
+        });
+    }
+}));
+
+app.get('/files', function(req, res) {
+	file.create([
+	    {
+	        name: "рарар",
+	        url: "http://google.com",
+	        semester: 3,
+	        type: "лекции"
+	    }], function (e, items) {
+	    	console.log(e)
+	});
+    res.send('hello world')
+})
+
+app.listen(8012)
