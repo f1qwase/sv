8930903c9584ebc284111077c63376a3151e03a7
timofeyka <timofeyka@gmail.com>
2013-04-20 16:57:27 +0400

    реструктуризация для работы в heroku

--- app.js
+++ app.js
@@ -1,37 +1,40 @@
-var files = [
-	{
-		name: "конспект по физике",
-		url: "http://google.com&q=конспект по физике",
-		subject: "физика",
-		semester: 4,
-		type: "лекция",
-		author: "Виктор Пепяка",
-		date: ""
-	},
-	{
-		name: "лаба по ЗИ",
-		url: "http://google.com&q=лаба по ЗИ",
-		subject: "защита информации",
-		semester: 4,
-		type: "задание",
-		author: "Андрей И",
-		date: ""
-	},
-	{
-		name: "моя лаба по ЗИ 1",
-		url: "http://google.com&q=моя лаба по ЗИ 1",
-		subject: "готовая работа",
-		semester: 4,
-		type: "лекция",
-		author: "John Doe",
-		date: ""
-	}
-]
-$(function(){
-	$.Mustache.load('./templates/templates.html')
-	    .done(function ( ) {
-	    	// console.log(arguments)
-	    	var fileListView = $.Mustache.render('fileList', files)
-	    	$("body").append(fileListView)
-  		})
+// var orm = require("orm")
+var express = require("express")
+var fs = require("fs")
+
+var app = express()
+
+// var config = JSON.parse(fs.readFileSync("config.json"))
+
+console.log("mysql://" + config.dbUser + ":" + config.dbPassword + "@" + config.dbHost + "/" + config.dbDatabase)
+
+// app.use(orm.express("mysql://" + config.dbUser + ":" + config.dbPassword + "@" + config.dbHost + "/" + config.dbDatabase, {
+//     define: function (db, models) {
+//         models.file = db.define("file", {
+//         	name: String,
+// 			url: String,
+// 			subject: String,
+// 			semester: Number,
+// 			type: ["лекции", "задание", "готовая работа", "вопросы", "другое"],
+// 			date: Number
+//         });
+//     }
+// }));
+
+app.get('/files', function(req, res) {
+	// file.create([
+	//     {
+	//         name: "рарар",
+	//         url: "http://google.com",
+	//         semester: 3,
+	//         type: "лекции"
+	//     }], function (e, items) {
+	//     	console.log(e)
+	// });
+    res.send('hello world')
 })
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
