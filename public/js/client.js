4de9b2b3a660fa2b768daa0ff4ccf73a55fcdfd2
timofeyka <timofeyka@gmail.com>
2013-06-07 20:14:03 +0400

    -Backbone. +jquery files load

--- public/js/client.js
+++ public/js/client.js
@@ -1,66 +1,6 @@
 var getStartFileList = function(callback) {
-	// $.getJSON("filesStub.json", callback
-	$.get("filesStub.json", {}, callback)
+	$.getJSON("/files/", {}, callback)
 }
-
-var File = Backbone.Model.extend({
-})
-
-var FileView = Backbone.View.extend({
-	model: File,
-	template: Mustache.compile($("#fileTpl").text()),
-	// initialize: function() {
-	// },
-	render: function() {
-		// console.log(this.model, this.template)
-		// return JSON.stringify(this.model.toJSON())
-		return this.template(this.model.toJSON())
-	}
-})
-
-var FilesCollection = Backbone.Collection.extend({
-	model: File,
-	url: "/files/",
-	initialize: function() {
-	}
-})
-
-FilesCollectionView = Backbone.View.extend({
-	template: "<div>hi, I'm template</div>",
-	initialize: function() {
-		this.collection.bind("reset", this.onReset, this)
-		this.collection.fetch({
-			reset: true
-		})
-	},
-	render: function(filter) {
-		var filter = filter || {}
-		console.log("render called")
-		var collectionView = this.collection.filter(function(file) {
-				for (var i in filter) {
-					//пока поиск объединется по "и"
-					if (file.get(i) != filter[i]) {
-						return false
-					}
-				}
-				return true
-			}).map(function(file) {
-				var fileView = new FileView({model: file})
-				return fileView.render()
-			}).join("")
-		return collectionView
-	},
-	onReset: function() {
-		console.log(this.render())
-		$("#accordion2").append(this.render())
-	}
-})
-
-
-
-
-
-
 
 $(function(){
 	$.Mustache.load('element_template.htm').done(function ( ) {
@@ -71,20 +11,13 @@ $(function(){
 			})
 		})
 	})
-	var filesCollectionView = new FilesCollectionView({
-		collection: new FilesCollection()
-	})
 })
 
-var getStartNavbar = function(callback) {
-	$.get("structure", {}, callback)
-}
 
 $(function(){
 	$.Mustache.load('navbar_template.htm').done(function ( ) {
-		getStartNavbar(function(data) {
+		$.getJSON("/structure/", function(data) {
 			var navbarView = $.Mustache.render('navbarView', data)
-			console.log(navbarView, data)
 			$("#navbar_position").append(navbarView)
 			$(".alert").alert();
 			$('.selectpicker').selectpicker();
@@ -99,5 +32,4 @@ $(function(){
 			})
 		})
 	})
-
 })
