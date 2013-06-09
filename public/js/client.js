ef847d2cc25eeecafb4c3d7776d35172a67819d8
timofeyka <timofeyka@gmail.com>
2013-06-10 03:13:11 +0400

    mega refactoring client.js

--- public/js/client.js
+++ public/js/client.js
@@ -12,15 +12,10 @@ $(document).bind("ajaxError", function(event, response, request) {
 	}
 })
 
-
-var getStartFileList = function(callback) {
-	$.getJSON("/files", {}, callback)
-}
-
-var searchFormat = function(key, value) {
+var tagFormat = function(key, value) {
 	return key + ":" + value
 }
-var searchDeformat = function(string) {
+var tagDeformat = function(string) {
 	var arr = string.split(":")
 	if (arr.length == 2) {
 		return {
@@ -29,6 +24,7 @@ var searchDeformat = function(string) {
 		}
 	}
 }
+
 
 var FilesView = function(params) {
 	$.extend(this, params)
@@ -40,17 +36,135 @@ FilesView.prototype = {
 	add: function(file) {
 		this.view.append(this.render(file))
 	},
-	reload: function(files) {
+	reload: function(parameters) {
 		var that = this
-		this.view.empty()
-		if (files.length == 0) {
-			this.view.append("нету ничего")
-		}
-		else {
-			files.forEach(function(file) {
-				that.add(file)
+		$.getJSON("/files", parameters || {}, function(files) {
+			that.view.empty()
+			if (files.length == 0) {
+				that.view.append("нету ничего")
+			}
+			else {
+				files.forEach(function(file) {
+					that.add(file)
+				})
+			}
+		})
+	}
+}
+
+var NavbarView = function(params) {
+	$.extend(this, params)
+	this.init()
+}
+
+NavbarView.prototype = {
+	init: function() {
+		var that = this
+		this.selectors = this.view.find("select")
+		this.selectors.selectpicker()
+		this.selectors.change(function(e) { //change tooltip
+			var tooltipText = $(this).siblings().find(".filter-option").text()
+			$(this).parent().attr("title", tooltipText)
+		})
+		this.selectors.change(function(e) {
+			var key = this.title
+			var options = $(this).find("option") 
+			var selected = options.filter(":selected").toArray().map(function(option) {
+				return $(option).val()
 			})
+			var notSelected = options.not(":selected").toArray().map(function(option) {
+				return $(option).val()
+			})
+			var existedTags = that.searchString.getTags()
+			selected.forEach(function(value) {
+				that.searchString.addTag(tagFormat(key, value))
+			})
+			notSelected.forEach(function(value) {
+				that.searchString.tryRemoveTag(tagFormat(key, value))
+			})
+		})
+
+		this.searchString.view.tagit({
+			afterTagRemoved: function(e, ui) {
+				var deletedTag = tagDeformat(ui.tagLabel)
+				if (deletedTag) {
+					that.updateOption(deletedTag.key, deletedTag.value, false)
+				}
+				that.searchString.go()
+			},
+			afterTagAdded: function(e, ui) {
+				var newTag = tagDeformat(ui.tagLabel)
+				if (newTag) {
+					that.updateOption(newTag.key, newTag.value, true)
+				}
+				that.searchString.go()
+			}
+		})
+	},
+	getSelector: function(name) {
+		return this.selectors.filter("[title='" + name + "']")
+	},
+	updateOption: function(selector, option, isSelected) {
+		var selector = this.getSelector(selector)
+		var optionView = selector.find("option:contains('" + option + "')")
+		isSelected ? optionView.attr("selected", "selected") : optionView.removeAttr("selected")
+		selector.selectpicker("refresh")
+	}
+}
+
+var SearchString = function(params) {
+	$.extend(this, params)
+	this.init()
+}
+
+SearchString.prototype = {
+	init: function() {
+		var that = this
+		this.view.tagit({
+			allowSpaces: true,
+			caseSensitive: false,
+			placeholderText: "Поиск",
+			singleFieldDelimiter: "\t"
+		})
+	},
+	dictionary: {
+		"Курс": "academicYear",
+		"Семестр": "semester",
+		"Предмет": "subject",
+		"Тип": "type",
+		"ss": "ss"
+	},
+	go: function() {
+		this.filesView.reload(this.asGetParams())
+	},
+	getTags: function() {
+		return this.view.tagit("assignedTags")
+	},
+	addTag: function(tag) {
+		return this.view.tagit("createTag", tag)
+	},
+	tryRemoveTag: function(tag) {
+		if (this.getTags().indexOf(tag) != -1) {
+			this.view.tagit("removeTagByLabel", tag)
 		}
+	},
+	asGetParams: function() {
+		var that = this
+		var parameters = {}
+		this.getTags().forEach(function(param) {
+			var tagsKeyValuePair = tagDeformat(param)
+			if (!tagsKeyValuePair) {
+				tagsKeyValuePair = {key: "ss", value: param}
+			}
+			parameters[tagsKeyValuePair.key] = parameters[tagsKeyValuePair.key] || []
+			parameters[tagsKeyValuePair.key].push(tagsKeyValuePair.value)
+
+		})
+		var getParameters = {}
+		for (var key in parameters) {
+			getParameters[this.dictionary[key]] = parameters[key].join(",")
+		}
+		return getParameters
 	}
 }
 
@@ -59,9 +173,7 @@ $(function() {
 		filesView = new FilesView({
 			view: $("#accordion2")
 		})
-		getStartFileList(function(fileList) {
-			filesView.reload(fileList)
-		})
+		filesView.reload({})
 	})
 })
 
@@ -69,90 +181,25 @@ $(function() {
 $(function(){
 	$.Mustache.load('navbar_template.htm').done(function ( ) {
 		$.getJSON("/structure/", function(data) {
-			var navbarView = $.Mustache.render('navbarView', data)
-			$("#navbar_position").append(navbarView)
+			$("#navbar_position").append($.Mustache.render('navbarView', data))
 			$(".alert").alert();
-			$('.selectpicker').selectpicker();
 
-			var searchString = $("#search-string")
-			$(".navbar select").change(function(e) { //change tooltip
-				var tooltipText = $(this).siblings().find(".filter-option").text()
-				$(this).parent().attr("title", tooltipText)
+			var searchString = new SearchString({
+				view: $("#search-string"),
+				filesView: filesView
 			})
 
-			$(".navbar select").change(function(e) {
-				var key = this.title
-				var options = $(this).find("option") 
-				var selected = options.filter(":selected").toArray().map(function(option) {
-					return $(option).val()
-				})
-				var notSelected = options.not(":selected").toArray().map(function(option) {
-					return $(option).val()
-				})
-				existedTags = searchString.tagit("assignedTags")
-				selected.forEach(function(value) {
-					searchString.tagit("createTag", searchFormat(key, value))
-				})
-				notSelected.filter(function(value) {
-					return existedTags.indexOf(searchFormat(key, value)) != -1
-				}).forEach(function(value) {
-					searchString.tagit("removeTagByLabel", searchFormat(key, value))
-				})
+			var navbarView = new NavbarView({
+				view: $(".navbar"),
+				searchString: searchString	
 			})
+		
 			deleteFile = function(id) {
 				$.ajax({
 					url: '/files/' + id,
 					type: 'DELETE',
 					success: searchString.go
 				})
-			}
-
-			searchString.go = function() {
-				$.getJSON("/files", searchString.asGetParams(), function(data) {
-					filesView.reload(data)
-				})
-			}
-			searchString.tagit({
-				allowSpaces: true,
-				hello: "asd",
-				caseSensitive: false,
-				placeholderText: "Поиск",
-				afterTagRemoved: function(e, ui) {
-					var deletedTag = searchDeformat(ui.tagLabel)
-					if (deletedTag) {
-						$(".navbar select[title='" + deletedTag.key + "'] option:contains('" + deletedTag.value + "')").removeAttr("selected")
-						$('.selectpicker').selectpicker('refresh');
-					}
-
-					searchString.go()
-				},
-				afterTagAdded: function(e, ui) {
-					searchString.go()
-				}
-			})
-			searchString.asGetParams = function() {
-				var dictionary = {
-					"Курс": "academicYear",
-					"Семестр": "semester",
-					"Предмет": "subject",
-					"Тип": "type",
-					"ss": "ss"
-				}
-				var parameters = {}
-				var tags = searchString.tagit("assignedTags").forEach(function(param) {
-					var tagsKeyValuePair = searchDeformat(param)
-					if (!tagsKeyValuePair) {
-						tagsKeyValuePair = {key: "ss", value: param}
-					}
-					parameters[tagsKeyValuePair.key] = parameters[tagsKeyValuePair.key] || []
-					parameters[tagsKeyValuePair.key].push(tagsKeyValuePair.value)
-
-				})
-				var getParameters = {}
-				for (var key in parameters) {
-					getParameters[dictionary[key]] = parameters[key].join(",")
-				}
-				return getParameters
 			}
 		})
 	})
