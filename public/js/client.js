463e3859705ce3053dbbbd2a9b5e06d8dc6870eb
timofeyka <timofeyka@gmail.com>
2013-06-09 14:59:26 +0400

    tagit added + some code cleanup

--- public/js/client.js
+++ public/js/client.js
@@ -2,6 +2,62 @@ var getStartFileList = function(callback) {
 	$.getJSON("/files/", {}, callback)
 }
 
+
+//{view(jQuery set)}
+// var SearchString = function(view) {
+// 	this.view = view
+// 	this.getText = function() {
+// 		return this.view.val()
+// 	}
+// 	this.setText = function(value) {
+// 		this.view.val(value)
+// 		return this
+// 	}
+// 	this.addText = function(value) {
+// 		var oldText = this.getText()
+// 		var newText = (oldText == "") ? value : oldText + ", " + value
+// 		this.setText(newText)
+// 	}
+// 	this.isSet = function(key, value) {
+// 		var isSetRegexp =  new RegExp(key + ": ?" + value)
+// 		return isSetRegexp.test(this.getText())
+// 	}
+// 	this.onDublicate = function(value) {
+// 		console.log("dublicate(" + value + ")")
+// 	}
+// 	this.add = function(key, value) {
+// 		if (!this.isSet(key, value)) {
+// 			this.addText(key + ": " + value)
+// 		}
+// 		else {
+// 			this.onDublicate(key + ": " + value)
+// 		}
+// 	}
+// }
+
+// SearchString.prototype = {
+// 	init: function() {
+// 		this.view.change(function(e) {
+
+// 		})
+// 	},
+// 	render: function() {
+// 		this.params.forEach(function(param))
+// 	},
+// 	change: function() {
+// 		this.view.text(this.render())
+// 	},
+// 	add: function(key, value) {
+// 		var _key = this.params[key] || []
+// 		if (inArray(_key, value) {
+// 			_key.push(value)
+// 		}
+// 		this.change()		
+// 	},
+// 	translate: function() {}
+
+// }
+
 $(function(){
 	$.Mustache.load('element_template.htm').done(function ( ) {
 		getStartFileList(function(data) {
@@ -30,6 +86,7 @@ $(function(){
 				// }).toArray()
 				// console.log(selected)
 			})
+			$("#search-string").tagit()
 		})
 	})
 })
