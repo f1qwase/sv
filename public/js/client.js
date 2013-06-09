57fdc92872469f3623ee8e8bad03e3f8b41b3c33
timofeyka <timofeyka@gmail.com>
2013-06-10 00:09:22 +0400

    query from search string, view reload on server response

--- public/js/client.js
+++ public/js/client.js
@@ -1,5 +1,5 @@
 var getStartFileList = function(callback) {
-	$.getJSON("/files/", {}, callback)
+	$.getJSON("/files", {}, callback)
 }
 
 var searchFormat = function(key, value) {
@@ -15,13 +15,32 @@ var searchDeformat = function(string) {
 	}
 }
 
-$(function(){
+$(function() {
+
 	$.Mustache.load('element_template.htm').done(function ( ) {
-		getStartFileList(function(data) {
-			data.forEach(function(file) {
-				var fileView = $.Mustache.render('fileView', file)
-				$("#accordion2").append(fileView)
-			})
+		filesView = {
+			render: function(file) {
+				return $.Mustache.render('fileView', file)
+			},
+			view: $("#accordion2"),
+			add: function(file) {
+				this.view.append(this.render(file))
+			},
+			reload: function(files) {
+				var that = this
+				this.view.empty()
+				if (files.length == 0) {
+					this.view.append("нету ничего")
+				}
+				else {
+					files.forEach(function(file) {
+						that.add(file)
+					})
+				}
+			}
+		}
+		getStartFileList(function(fileList) {
+			filesView.reload(fileList)
 		})
 	})
 })
@@ -57,29 +76,59 @@ $(function(){
 				selected.forEach(function(value) {
 					searchString.tagit("createTag", searchFormat(key, value))
 				})
-				console.log(notSelected, existedTags)
 				notSelected.filter(function(value) {
 					return existedTags.indexOf(searchFormat(key, value)) != -1
 				}).forEach(function(value) {
-					console.log("rm " + value)
 					searchString.tagit("removeTagByLabel", searchFormat(key, value))
 				})
 			})
 
 			searchString.tagit({
 				allowSpaces: true,
+				hello: "asd",
 				caseSensitive: false,
 				placeholderText: "Поиск",
 				afterTagRemoved: function(e, ui) {
 					var deletedTag = searchDeformat(ui.tagLabel)
 					if (deletedTag) {
-						console.log(deletedTag)
-						console.log($(".navbar select[title='" + deletedTag.key + "'] option:contains('" + deletedTag.value + "')"))
 						$(".navbar select[title='" + deletedTag.key + "'] option:contains('" + deletedTag.value + "')").removeAttr("selected")
 						$('.selectpicker').selectpicker('refresh');
 					}
+
+					$.getJSON("/files", searchString.asGetParams(), function(data) {
+						filesView.reload(data)
+					})
+				},
+				afterTagAdded: function(e, ui) {
+					$.getJSON("/files", searchString.asGetParams(), function(data) {
+						filesView.reload(data)
+					})
 				}
 			})
+			searchString.asGetParams = function() {
+				var dictionary = {
+					"Курс": "academicYear",
+					"Семестр": "semester",
+					"Предмет": "subject",
+					"Тип": "type"
+				}
+				var parameters = {}
+				var tags = searchString.tagit("assignedTags").forEach(function(param) {
+					var tagsKeyValuePair = searchDeformat(param)
+					if (!tagsKeyValuePair) {
+						tagsKeyValuePair = {key: "ss", value: param}
+					} 
+					parameters[tagsKeyValuePair.key] = parameters[tagsKeyValuePair.key] || []
+					parameters[tagsKeyValuePair.key].push(tagsKeyValuePair.value)
+				})
+				// console.log(parameters)
+				// console.log(searchString)
+				var getParameters = {}
+				for (var key in parameters) {
+					getParameters[dictionary[key]] = parameters[key].join(",")
+				}
+				return getParameters
+			}
 
 
 		})
